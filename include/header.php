31766e6215b9061105545421320265b9259a2f97
Alamdesignergithum <94550942+Alamdesignergithum@users.noreply.github.com>
2021-11-17 21:29:12 +0530

    update commit

---
+++ include/header.php
@@ -0,0 +1,114 @@
+
+<link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+<link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" >
+<link rel="stylesheet" href="assets/css/style.css" >
+<link rel="stylesheet" href="assets/fonts/icofont.min.css" >
+<link rel="stylesheet" href="assets/css/animate.min.css" >
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark cutom-header">
+  <div class="container">
+    <a class="navbar-brand" href="index.php"><img src="assets/images/logo.svg" class="brand-logo"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarsExample07">
+      <ul class="navbar-nav ml-auto custome-munu">
+        <li class="nav-item dropdown position-statics">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Home</a>
+          <div class="dropdown-menu hover-menu animate__animated animate__fadeInUp pt-3" aria-labelledby="dropdown07">
+            <div class="row">
+              <div class="col-md-3 col-6 mb-3">
+                <h5 class="pl-2 mt-0 mb-2 bold text-white">Digital Marketing</h5>
+                <ul class="mega-menu-list border-top">
+                  <li><a href="digital-marketing-services.php">Digital Marketing</a></li>
+                  <li><a href="search-engine-optimization-seo.php">Search Engine Optimization</a></li>
+                  <li><a href="local-seo.php">Local SEO</a></li>
+                  <li><a href="technical-seo">Technical SEO</a></li>
+                  <li><a href="digital-marketing-strategy">Digital Marketing Strategy</a></li>
+                  <li><a href="pay-per-click-ppc">Pay Per Click Marketing</a></li>
+                  <li><a href="link-building">Link Building</a></li>
+                  <li><a href="google-analytics-service">Google Analytics Service</a></li>
+                  <li><a href="conversion-rate-optimization">Conversion Rate Optimization</a></li>
+                  <li><a href="white-label-services">White Label Services</a></li>
+                  <li><a href="franchise-seo">Franchise SEO</a></li>
+                  <li><a href="search-engine-marketing">Search Engine Marketing</a></li>
+                </ul>
+              </div>
+
+              <div class="col-md-3 col-6 mb-3">
+                <h5 class="pl-2 mt-0 mb-2 bold text-white">Web Design</h5>
+                <ul class="mega-menu-list border-top">
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                </ul>
+              </div>
+
+              <div class="col-md-3 col-6 mb-3">
+                <h5 class="pl-2 mt-0 mb-2 bold text-white">Social Media Marketing</h5>
+                <ul class="mega-menu-list border-top">
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                </ul>
+              </div>
+
+              <div class="col-md-3 col-6 mb-3">
+                <h5 class="pl-2 mt-0 mb-2 bold text-white">eCommerce Marketing</h5>
+                <ul class="mega-menu-list border-top">
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Home</a></li>
+                </ul>
+              </div>
+
+            </div>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Service</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Work</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Resource</a>
+          <div class="dropdown-menu sm-hover animate__animated animate__fadeInUp" aria-labelledby="dropdown07">
+            <!-- <a class="dropdown-item" href="#">Servivces1</a>
+            <a class="dropdown-item" href="#">Servivces2</a>
+            <a class="dropdown-item" href="#">Servivces3</a> -->
+            <ul class="mega-menu-list">
+              <li><a href="index.php">Home</a></li>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Home</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+      <div class="call-us ml-5 d-md-block d-none">
+        <i class="icofont-phone icofont-1x green-color"></i>
+        <a href="tel:xxx-xxx-xxxx" class="bold blue-color"> <strong>+1-222-333-4560</strong></a>
+        <!-- <i class="icofont-search-2 icofont-2x green-color"></i> -->
+      </div><!--/call-us-->
+    </div>
+  </div>
+</nav>
